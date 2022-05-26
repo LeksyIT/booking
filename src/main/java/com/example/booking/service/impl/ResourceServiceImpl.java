8f26aaa6e2a7390ed655eaa607982d9f2520cfd0
leksyit <leksymonit@gmail.com>
2022-05-26 19:14:36 +0300

    append ResourceService and ResourceRepository

---
+++ src/main/java/com/example/booking/service/impl/ResourceServiceImpl.java
@@ -0,0 +1,10 @@
+package com.example.booking.service.impl;
+
+import com.example.booking.service.ResourceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ResourceServiceImpl implements ResourceService {
+}
